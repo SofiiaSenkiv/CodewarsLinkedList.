c87b8d97e68bbfe140206a54f590cf58e9308cd6
SofiiaSenkiv <senkivsofijka@gmail.com>
2024-04-06 21:55:36 +0300

    created function

---
+++ remove_dublicates.py
@@ -0,0 +1,15 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def remove_duplicates(head):
+    # Your code goes here.
+    # Remember to return the head of the list.
+    current = head
+    while current.next:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+    return head
