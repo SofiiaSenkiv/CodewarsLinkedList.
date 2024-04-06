555db14fc07b1d84329233fbb8195233ab44537b
SofiiaSenkiv <senkivsofijka@gmail.com>
2024-04-06 21:27:18 +0300

    added check

--- sorted_insert.py
+++ sorted_insert.py
@@ -7,6 +7,9 @@ def sorted_insert(head, data):
     # Your code goes here.
     # Make sure to return the head of the list.
     new_node = Node(data)
+    if head is None or data < head.data:
+        new_node.next = head
+        return new_node
     current = head
     while current.next and current.next.data < data:
         current = current.next
