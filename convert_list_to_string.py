125010777162226784b0ffb2026b12d707292091
SofiiaSenkiv <senkivsofijka@gmail.com>
2024-04-06 19:57:38 +0300

    completed function and added photo

--- convert_list_to_string.py
+++
@@ -1,8 +0,0 @@
-def stringify(lst):
-    result = ""
-    current = lst
-    while current:
-        result += str(current.data) + " -> "
-        current = current.next
-    result += "None"
-    return result
