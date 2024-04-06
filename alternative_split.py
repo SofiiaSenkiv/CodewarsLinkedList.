a6e7946ebd82e70a898f105317e31f877f84e2b5
SofiiaSenkiv <senkivsofijka@gmail.com>
2024-04-07 00:55:40 +0300

    added function

---
+++ alternative_split.py
@@ -0,0 +1,26 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    
+def alternating_split(head):
+    # Your code goes here.
+    # Remember to return the context.
+    first_head = head
+    second_head = head.next
+    first_current = first_head
+    second_current = second_head
+
+    while first_current and second_current:
+        first_current.next = second_current.next
+        if first_current.next:
+            second_current.next = first_current.next.next
+        first_current = first_current.next
+        second_current = second_current.next
+
+    return Context(first_head, second_head)
